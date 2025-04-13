d2cbb64c7351afc1d85398ee732137e7c6bdf572
leandroJader <ldandro>
2025-04-13 12:48:08 -0300

    Add project files.

---
+++ Projeto_Clube_do_livro_2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_Clube_do_livro_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
